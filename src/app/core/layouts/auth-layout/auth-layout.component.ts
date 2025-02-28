da19da1fb8c2fdbdab46179f3cbf1e886f2f42b8
Rana-hanii <ranahani797@gmail.com>
2025-02-28 17:01:55 +0200

    s

--- src/app/core/layouts/auth-layout/auth-layout.component.ts
+++ src/app/core/layouts/auth-layout/auth-layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
